8e8ad0b55f37a55874de22e703142b7ff0ee7793
txj-xyz <jfinelli3@gmail.com>
2022-09-20 15:21:13 +0000

    new rework mastery command for caching

--- src/interactions/mastery/Mastery.ts
+++ src/interactions/mastery/Mastery.ts
@@ -1,6 +1,7 @@
 import BotInteraction from '../../types/BotInteraction';
-import { ChatInputCommandInteraction, Guild, Collection, GuildMember, Role, SlashCommandBuilder } from 'discord.js';
+import { ChatInputCommandInteraction, Guild, Collection, GuildMember, Role, SlashCommandBuilder, SlashCommandBooleanOption } from 'discord.js';
 import type { MasteryData } from '../../types/MasteryData';
+// import { PaginatedMessage } from '@sapphire/discord.js-utilities';
 
 export default class Mastery extends BotInteraction {
     get name() {
@@ -12,11 +13,14 @@ export default class Mastery extends BotInteraction {
     }
 
     get permissions() {
-        return 'OWNER';
+        return 'staff';
     }
 
     get slashData() {
-        return new SlashCommandBuilder().setName(this.name).setDescription(this.description);
+        return new SlashCommandBuilder()
+            .setName(this.name)
+            .setDescription(this.description)
+            .addBooleanOption((option: SlashCommandBooleanOption) => option.setName('cache').setDescription('Force Fetch cache data').setRequired(true));
     }
 
     // get options() {
@@ -30,16 +34,22 @@ export default class Mastery extends BotInteraction {
     //     ];
     // }
 
-    private async memberCache(): Promise<Guild | undefined> {
-        const guild: Guild | undefined = this.client.guilds.cache.get(this.client.util.config.pvmeData.pvme_guild_id);
-        const members: Collection<string, GuildMember> | undefined = await guild?.members.fetch();
-        if (guild && members) return guild;
-        else return undefined;
+    private async memberCache(option: boolean | null): Promise<Guild | undefined> {
+        if (option) {
+            const guild: Guild | undefined = this.client.guilds.cache.get(this.client.util.config.pvmeData.pvme_guild_id);
+            const members: Collection<string, GuildMember> | undefined = await guild?.members.fetch();
+            if (guild && members) return guild;
+            else return undefined;
+        } else {
+            const guild: Guild | undefined = this.client.guilds.cache.get(this.client.util.config.pvmeData.pvme_guild_id);
+            return guild;
+        }
     }
 
-    private async roleCache(): Promise<Collection<string, Role> | null> {
-        const _memberCache = await this.memberCache();
+    private async roleCache(option: boolean | null): Promise<Collection<string, Role> | null> {
+        const _memberCache = await this.memberCache(option);
         const roles = _memberCache?.roles.cache;
+        // console.log(roles);
         return roles ?? null;
     }
 
@@ -52,51 +62,46 @@ export default class Mastery extends BotInteraction {
             initiate: [],
             novice: [],
         };
-
-        const _cache: Role[] = role_cache.map((role) => role);
-        for await (const role of _cache) {
-            if (role.name.toLowerCase().includes('grandmaster')) {
-                mapped_stats.grandmaster.push(`${role.name}: '${role.members.size}' members.`);
-            } else if (role.name.includes(' Master')) {
-                mapped_stats.master.push(`${role.name}: '${role.members.size}' members.`);
-            } else if (role.name.toLowerCase().includes('adept')) {
-                mapped_stats.adept.push(`${role.name}: '${role.members.size}' members.`);
-            } else if (role.name.toLowerCase().includes('apprentice')) {
-                mapped_stats.apprentice.push(`${role.name}: '${role.members.size}' members.`);
-            } else if (role.name.toLowerCase().includes('initiate')) {
-                mapped_stats.initiate.push(`${role.name}: '${role.members.size}' members.`);
-            } else if (role.name.toLowerCase().includes('novice')) {
-                mapped_stats.novice.push(`${role.name}: '${role.members.size}' members.`);
-            }
+        const _grandmaster = role_cache.filter((role) => role.name.toLowerCase().includes('grandmaster'));
+        for await (const [, role] of _grandmaster) {
+            mapped_stats.grandmaster.push(`${role.name}: '${role.members.size}' members.`);
+        }
+        const _master = role_cache.filter((role) => role.name.includes(' Master'));
+        for await (const [, role] of _master) {
+            mapped_stats.master.push(`${role.name}: '${role.members.size}' members.`);
+        }
+        const _adept = role_cache.filter((role) => role.name.toLowerCase().includes('adept'));
+        for await (const [, role] of _adept) {
+            mapped_stats.adept.push(`${role.name}: '${role.members.size}' members.`);
+        }
+        const _apprentice = role_cache.filter((role) => role.name.toLowerCase().includes('apprentice'));
+        for await (const [, role] of _apprentice) {
+            mapped_stats.apprentice.push(`${role.name}: '${role.members.size}' members.`);
+        }
+        const _initiate = role_cache.filter((role) => role.name.toLowerCase().includes('initiate'));
+        for await (const [, role] of _initiate) {
+            mapped_stats.initiate.push(`${role.name}: '${role.members.size}' members.`);
+        }
+        const _novice = role_cache.filter((role) => role.name.toLowerCase().includes('novice'));
+        for await (const [, role] of _novice) {
+            mapped_stats.novice.push(`${role.name}: '${role.members.size}' members.`);
         }
 
         return mapped_stats;
     }
 
     async run(interaction: ChatInputCommandInteraction) {
-        if (interaction.inCachedGuild()) {
-            await interaction.deferReply({ ephemeral: false });
-            const _canUseCommand: Collection<string, Role> = interaction.member?.roles.cache.filter((role) => role.id.includes(this.client.util.config.pvmeData.pvme_staff_id));
-            if (!_canUseCommand.size) {
-                await interaction.editReply(`You must ask PvME Staff to pull this report.`);
-                return;
-            }
-            await interaction.editReply(`Loading role cache...`);
-            const roles: Collection<string, Role> | null = await this.roleCache();
-            await interaction.editReply(`Done... ${roles?.size} roles found`);
-            if (roles) {
-                await interaction.editReply(`Starting to parse role mastery data... <a:majjnow:1006284731928805496>`);
-                const _mastery = await this.parseRoles(roles);
-                await interaction.editReply(`Loaded data, sending...`);
-                await interaction.followUp(`\`\`\`json\nGrandmaster: ${_mastery.grandmaster.join('\n')}\n\`\`\``);
-                await interaction.followUp(`\`\`\`json\nMaster: ${_mastery.master.join('\n')}\n\`\`\``);
-                await interaction.followUp(`\`\`\`json\nAdept: ${_mastery.adept.join('\n')}\n\`\`\``);
-                await interaction.followUp(`\`\`\`json\nApprentice: ${_mastery.apprentice.join('\n')}\n\`\`\``);
-                await interaction.followUp(`\`\`\`json\nInitiate: ${_mastery.initiate.join('\n')}\n\`\`\``);
-                await interaction.followUp(`\`\`\`json\nNovice: ${_mastery.novice.join('\n')}\n\`\`\``);
-            } else {
-                await interaction.editReply(`There was an error retrieving the role data, contact TXJ`);
-                return;
+        if (!interaction.inCachedGuild()) return void 0;
+        const option = interaction.options.getBoolean('cache');
+        await interaction.deferReply({ ephemeral: false });
+        const roles: Collection<string, Role> | null = await this.roleCache(option ?? null);
+        if (roles) {
+            // const page = new this.client.util.paginate();
+            // const page = new PaginatedMessage();
+            const m = await this.parseRoles(roles);
+            const mr = Object.keys(m).map((c) => ({ name: `${c.slice(0, 1).toUpperCase()}${c.slice(1, c.length)}`, roles: m[c as keyof MasteryData] })) as { name: string; roles: string[] }[];
+            for (const r of mr) {
+                await interaction.followUp(`${r.name}:\n\`\`\`json\n${r.roles.join('\n')}\`\`\``);
             }
         }
     }
