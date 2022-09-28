41e7db762d41b5977f1738b278dd345b60a464ad
txj-xyz <jfinelli3@gmail.com>
2022-09-28 18:21:52 +0000

    dev: cleanup

--- src/interactions/pvme/Forum.ts
+++ src/interactions/pvme/Forum.ts
@@ -1,6 +1,6 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder, EmbedBuilder, ChannelType, Message } from 'discord.js';
-// import { ApplicationCommandOptionType } from 'discord-api-types/v9';
 import BotInteraction from '../../types/BotInteraction';
+
 export default class Forum extends BotInteraction {
     count = 0;
     count_blank = 0;
@@ -120,37 +120,43 @@ export default class Forum extends BotInteraction {
     private async convertForumPost(interaction: ChatInputCommandInteraction) {
         this._posts_converted = [];
         if (!interaction.inCachedGuild()) return;
-        // await interaction.deferReply({ ephemeral: false });
         const _forum = interaction.options.getChannel('forum_channel', true);
-
         const _delete_old_posts = interaction.options.getBoolean('delete_posts', true);
         const _ignore_archived_posts = interaction.options.getBoolean('ignore_archived', true);
-
         const _get_cached_channel = interaction.guild?.channels.cache.get(_forum.id);
+
         // Send message if the channel requested is not a forum channel.
         const _non_forum_channel_embed = new EmbedBuilder()
             .setColor(this.client.color)
             .setDescription(`**${_forum}** is not a forum channel.`)
             .setTimestamp()
             .setFooter({ text: this.client.user?.username ?? 'dejj', iconURL: this.client.user?.displayAvatarURL() });
+
+        // Defer Reply
         await interaction.deferReply({ ephemeral: false });
+
+        // Ignore channel if not a ChannelType.GuildForum stop here
         if (_get_cached_channel?.type !== ChannelType.GuildForum) return interaction.editReply({ embeds: [_non_forum_channel_embed] });
 
-        // force the new cache to store archived threads too
+        // force the new cache to store archived threads too if the user opted in
         _ignore_archived_posts ? void 0 : await _get_cached_channel.threads.fetchArchived({}, true); // MANAGE_THREADS PERM REQUIRED
         await _get_cached_channel.threads.fetchActive(true);
 
         // get all threads from the mentioned forum
         const _get_cached_threads = _get_cached_channel.threads;
         const _mapped_user_posts = _get_cached_threads.cache.filter((post) => post.ownerId !== this.client.user?.id);
+
+        // if there is nothing to convert then stop here
         if (_mapped_user_posts.size === 0) return interaction.editReply({ content: `There are no posts to convert` });
 
+        // Loop through the posts ready to convert
         for await (const [old_post_id, post] of _mapped_user_posts) {
+            // Fetch the first message of each post
             const _first_message: Message<any> | undefined | null = await post.fetchStarterMessage({ cache: false }).catch((e) => undefined);
             if (_first_message) {
+                // Check conditions for each post type clause
                 const _attachment = _first_message?.attachments?.first();
                 const _has_content = _first_message?.content ? true : false;
-                this.count++;
                 if (_attachment && _has_content) {
                     // has attachment and initial message content
                     const __post = await _get_cached_channel.threads.create({
@@ -159,6 +165,7 @@ export default class Forum extends BotInteraction {
                         autoArchiveDuration: 10080,
                         message: _attachment ? { content: _first_message?.content, files: [_attachment] } : { content: _first_message?.content },
                     });
+                    // push the conversion data to a temp array
                     this._posts_converted.push({ name: __post.name, old_id: old_post_id, new_id: __post.id });
                     _delete_old_posts ? post.delete() : void 0;
                 } else if (!_attachment && _has_content) {
@@ -169,6 +176,7 @@ export default class Forum extends BotInteraction {
                         autoArchiveDuration: 10080,
                         message: { content: _first_message?.content },
                     });
+                    // push the conversion data to a temp array
                     this._posts_converted.push(new Object({ name: __post.name, old_id: old_post_id, new_id: __post.id }));
                     _delete_old_posts ? post.delete() : void 0;
                 }
@@ -196,12 +204,6 @@ export default class Forum extends BotInteraction {
             .setFooter({ text: this.client.user?.username ?? 'dejj', iconURL: this.client.user?.displayAvatarURL() });
 
         interaction.followUp({ embeds: [__posts_converted_embed] });
-
-        // const _cache_threads = this.client.guilds.cache
-        //     .get('918662128632733696')
-        //     .channels.cache.get('1021892943218483230')
-        //     .threads.cache.map((post) => new Object({ name: post.name, id: post.id, isBotUser: post.ownerId !== '1015008038823931994' }));
-        // interaction.editReply(_forum?.name);
     }
 
     private async createForumPost(interaction: ChatInputCommandInteraction) {
@@ -246,8 +248,6 @@ export default class Forum extends BotInteraction {
         }
     }
 
-    // private async convertForumPost(interaction: ChatInputCommandInteraction) {}
-
     async run(interaction: ChatInputCommandInteraction) {
         if (!interaction.inCachedGuild() && !interaction.isChatInputCommand()) return this.client.logger.error({ error: `There was an error with **${this.constructor.name}**` });
         const _subcommand = interaction.options.getSubcommand(true);
@@ -261,9 +261,6 @@ export default class Forum extends BotInteraction {
             case 'convert':
                 await this.convertForumPost(interaction);
                 break;
-            // case 'copy':
-            //     await this.convertForumPost(interaction);
-            //     break;
             default:
                 interaction.editReply(`Subcommand not defined`);
                 break;
