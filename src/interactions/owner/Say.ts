512b025d1a9242be29ab96edbed467e147e7132a
txj-xyz <jfinelli3@gmail.com>
2022-10-11 17:20:06 +0000

    allow an image attachment for `say` command

--- src/interactions/owner/Say.ts
+++ src/interactions/owner/Say.ts
@@ -1,5 +1,5 @@
 import BotInteraction from '../../types/BotInteraction';
-import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+import { Attachment, ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
 
 export default class Say extends BotInteraction {
     get name() {
@@ -18,13 +18,17 @@ export default class Say extends BotInteraction {
         return new SlashCommandBuilder()
             .setName(this.name)
             .setDescription(this.description)
-            .addStringOption((option) => option.setName('message').setDescription('The message you want to send.').setRequired(true));
+            .addStringOption((option) => option.setName('message').setDescription('The message you want to send.').setRequired(true))
+            .addAttachmentOption((option) => option.setName('image').setDescription('The image attachment you want to send.').setRequired(false));
     }
 
     async run(interaction: ChatInputCommandInteraction) {
         await interaction.deferReply({ ephemeral: true });
-        const message = interaction.options.getString('message', true);
-        await interaction.channel?.send(message);
+        const message: string = interaction.options.getString('message', true);
+        const attachment: Attachment | null = interaction.options.getAttachment('image', false);
+        // const role: Role | APIRole | null = interaction.options.getRole('role_search', false);
+        // const _role_members = interaction.guild?.roles.cache.get(role?.id)?.members.size;
+        await interaction.channel?.send(attachment ? { content: message, files: [attachment] } : { content: message });
         await interaction.editReply(`Sent Message: \`\`\`\n${message}\n\`\`\``);
     }
 }
