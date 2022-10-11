739420c4ac6c54886dbcf99d22a5f554d39a46fc
txj-xyz <jfinelli3@gmail.com>
2022-10-11 14:19:40 +0000

    fetch member cache on start

--- src/events/Ready.ts
+++ src/events/Ready.ts
@@ -1,4 +1,5 @@
 // import Bot from '../Bot';
+import { Guild } from 'discord.js';
 import Bot from '../Bot';
 import BotEvent from '../types/BotEvent';
 export default class Ready extends BotEvent {
@@ -20,7 +21,11 @@ export default class Ready extends BotEvent {
 
     //test comment
     async run(client: Bot) {
-        this.client.logger.log({ message: `[${this.client.user?.username}] Ready! Serving ${this.client.guilds.cache.size} guild(s) with ${this.client.users.cache.size} user(s)` });
+        // fetch user cache
+        const guild: Guild | undefined = this.client.guilds.cache.get(this.client.util.config.pvmeData.pvme_guild_id);
+        await guild?.members.fetch();
+
+        this.client.logger.log({ message: `[${this.client.user?.username}] Ready! Serving ${this.client.guilds.cache.size} guild(s) with ${this.client.users.cache.size} user(s)` }, true);
         this.client.user?.setActivity(`with guides. '@PvME boop'`);
         setInterval((): void => {
             const current = this.statuses.shift() ?? '';
